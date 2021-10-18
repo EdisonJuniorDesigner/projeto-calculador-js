933233a8572b93352573a296a8ca1afee12bbb7f
EdisonJuniorDesigner <mage1232010@gmail.com>
2021-10-18 20:54:18 -0300

    calculadora simples finalizada

--- scripts.js
+++ scripts.js
@@ -1,15 +1,22 @@
-function insert(num){
+function insert(num) {
     let numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
 
-function clean(){
+function clean() {
     document.getElementById('resultado').innerHTML = "";
 }
 
-function back(){
+function back() {
     let resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
 }
 
-function calcular(){}
+function calcular() {
+    let resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    } else {
+        document.getElementById('resultado').innerHTML = "Nada...";
+    }
+}
